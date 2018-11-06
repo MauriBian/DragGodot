57f89cad242d631527810ce3435af48fb23517a8
MauriBian <mauricioosvaldobianchi@gmail.com>
2018-11-06 14:20:27 -0300

    pick

--- Scripts/Objetos.gd
+++ Scripts/Objetos.gd
@@ -1,8 +1,10 @@
 extends Area2D
 var startDrag = false
-
+var startPosition
+var stay
 func _ready():
-    set_process_input(true)
+	startPosition = self.position
+	set_process_input(true)
 
 
 func _input_event(viewport, event, shape_idx):
@@ -10,10 +12,18 @@ func _input_event(viewport, event, shape_idx):
 		startDrag = true	
 	if event.is_action_released("LeftClick") :
 		startDrag = false
-		
+		if !stay:
+			self.position = startPosition
 func Drag():
 	if startDrag:
 		self.position = get_viewport().get_mouse_position()
 	
 func _process(delta):
 	Drag()
+
+func _on_Book1_body_entered(body):
+	stay = true
+
+
+func _on_Book1_body_exited(body):
+	stay = false
